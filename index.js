98b3d40722d4dcdc55f5976bf4f6583e74569dd1
Hilmi AM <imiia75775@proton.me>
2024-09-04 16:09:13 +0700

    script

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-import {writeToFile} from "./write.js";
+// import {writeToFile} from "./write.js";
 
-writeToFile("me.log", "NODE NODE NODE\n YES YES YES")
+// writeToFile("me.log", "NODE NODE NODE\n YES YES YES")
+
+console.info("Hello World")
